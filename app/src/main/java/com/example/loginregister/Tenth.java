4eca5d069087c52b0a4a5de9b0d5074cb1c17855
NEETU SAHU <neetusahu183@gmail.com>
2026-06-09 23:49:06 +0530

    services page updated

--- app/src/main/java/com/example/loginregister/Tenth.java
+++ app/src/main/java/com/example/loginregister/Tenth.java
@@ -1,6 +1,10 @@
 package com.example.loginregister;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Tenth extends AppCompatActivity {
+    Button b20,b21,b22;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tenth);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        b20=(Button) findViewById(R.id.button20);
+        b21=(Button) findViewById(R.id.button21);
+        b22=(Button) findViewById(R.id.button22);
+        b20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Tenth.this, Eleventh.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        b21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent g=new Intent(Tenth.this, Twelfth.class);
+                startActivity(g);
+                finish();
+            }
+        });
+        b22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent h=new Intent(Tenth.this, Thirteenth.class);
+                startActivity(h);
+                finish();
+            }
         });
     }
 }
